be409f2476b27cd656883cda369c2dc939c878a4
Your Name <yasielpv@sd.sd>
2025-08-25 08:40:04 -0400

    add fields

--- src/components/common/forms/fields/ImageUploadInput.tsx
+++ src/components/common/forms/fields/ImageUploadInput.tsx
@@ -10,8 +10,10 @@ import {
   FormControl,
   Paper,
   Typography,
+  Card,
+  CardContent,
 } from '@mui/material';
-import { CloudUpload, Delete } from '@mui/icons-material';
+import { CloudUpload, CloudUploadOutlined, Delete, DeleteOutline, Upload } from '@mui/icons-material';
 import { useTranslations } from 'next-intl';
 import MediaModalSelectedFiles from '../../modals/MediaModalSelectedFiles/MediaModalSelectedFiles';
 import { useCommonModal } from '@/hooks/useCommonModal';
@@ -19,21 +21,20 @@ import { CommonModalType } from '@/contexts/commonModalContext';
 import { IUserMedia, IUserMediaType } from '@/domain/core/IUserMedia';
 import ImagePreview from '../../ImagePreview';
 import { fileTypes } from '@/config/constants';
+import { SassButton } from '../../buttons/GenericButton';
 
 interface ImageFieldProps {
   accept: string
   typeUpload: IUserMediaType
 }
-const ImageUploadInput = ({ name, label, ...props }: any & FieldProps & TextFieldProps & ImageFieldProps) => {
+const ImageUploadInput = ({ name, ...props }: any & FieldProps & TextFieldProps & ImageFieldProps) => {
   const t = useTranslations();
   const [field, meta, helper] = useField(name);
   const { touched, error } = meta
   const typeUpload = props.type
 
-  const aspectRatio = (fileTypes(t).find(e => e.value === typeUpload)?.size?.w ?? 0) / (fileTypes(t).find(e => e.value === typeUpload)?.size?.h ?? 0)
-  const width = 300
-  const height = width / aspectRatio;
-  const isDragging = false
+  const width = fileTypes(t).find(e => e.value === typeUpload)?.size?.boxW || 120;
+  const height = fileTypes(t).find(e => e.value === typeUpload)?.size?.boxH || 120;;
   const helperText = touched && error;
   const { open, openModal } = useCommonModal()
   const {
@@ -65,59 +66,60 @@ const ImageUploadInput = ({ name, label, ...props }: any & FieldProps & TextFiel
 
   return (
     <FormControl key={name} required sx={{ m: 1, width: '100%' }}>
-      <InputLabel error={!!helperText} id="demo-simple-select-required-label">{label}</InputLabel>
+
       <Box sx={{ paddingTop: 5 }}>
-        {preview ? (
-          <Box sx={{ position: 'relative', display: 'inline-block', borderRadius: '1px solid red' }}>
-            <ImagePreview
-              src={preview}
-              alt=""
-              width={'200px'}
-              height={'200px'}
-              style={{
-                width: '100%',
-                height: height,
-                border: '1px solid #ddd'
-              }}
-              zoomIconPosition="center"
-            />
-            <IconButton disabled={props.disabled}
-              onClick={handleRemoveImage}
-              sx={{
-                position: 'absolute',
-                top: 0,
-                right: 0,
-                backgroundColor: 'rgba(255, 255, 255, 0.7)',
-                '&:hover': {
-                  backgroundColor: 'rgba(255, 255, 255, 0.9)',
-                },
-              }}
-            >
-              <Delete color="error" />
-            </IconButton>
+        {preview ? (<Box display={'flex'} flexDirection={'column'} alignItems={'center'} gap={1}>
+          <Card elevation={1} >
+            <CardContent>
+              <Box display={'flex'} flexDirection={'column'} alignItems={'center'} gap={1}>
+                <Typography textTransform={'uppercase'}>{t('core.label.' + typeUpload)}</Typography>
+                <Box sx={{ position: 'relative', display: 'inline-block', borderRadius: '1px solid red' }}>
+                  <ImagePreview
+                    src={preview}
+                    alt=""
+                    width={width + 'px'}
+                    height={height + 'px'}
+
+                    zoomIconPosition="center"
+                  />
+                </Box>
+              </Box>
+              <Typography variant='caption'>{t('core.label.medida')}: {fileTypes(t).find(e => e.value === typeUpload)?.size.w} x {fileTypes(t).find(e => e.value === typeUpload)?.size.h} px. PNG.</Typography>
+              </CardContent>
+          </Card>
+          <Box display={'flex'} gap={1} sx={{ pt: 1 }}>
+            <SassButton sx={{ height: 35, textTransform: 'capitalize' }} onClick={() => openModal(CommonModalType.FILES, { name })} size='small' variant='contained' color='primary' startIcon={<Upload />}>
+              {t('core.button.replaceImage')}
+            </SassButton>
+            <SassButton sx={{ height: 35, textTransform: 'capitalize' }} onClick={handleRemoveImage} size='small' variant='contained' color='secondary' startIcon={<DeleteOutline />}>
+              {t('core.button.delete')}
+            </SassButton>
           </Box>
-        ) : (
-          <Box sx={{ width: '100%', maxWidth: width }}>
+        </Box>) : (
+
+
+          <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 1 }}>
+            <Typography textTransform={'uppercase'}>{t('core.label.' + typeUpload)}</Typography>
             <Paper onClick={() => openModal(CommonModalType.FILES, { name })}
               variant="outlined"
               sx={{
                 position: 'relative',
-                width: '100%',
+                width: width,
                 height: height,
-                border: isDragging ? '2px dashed #1976d2' : '2px dashed #ccc',
+                border: '2px dashed #476BE7',
                 borderRadius: 2,
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
                 justifyContent: 'center',
-                backgroundColor: isDragging ? 'rgba(25, 118, 210, 0.04)' : 'transparent',
+                background: 'rgba(0, 84, 202, 0.08)',
                 cursor: props.disabled ? 'not-allowed' : 'pointer',
                 opacity: props.disabled ? 0.6 : 1,
                 overflow: 'hidden',
                 transition: 'all 0.2s ease-in-out',
                 '&:hover': {
-                  borderColor: props.disabled ? '#ccc' : '#1976d2',
-                  backgroundColor: props.disabled ? 'transparent' : 'rgba(25, 118, 210, 0.04)'
+                  borderColor: props.disabled ? '#ccc' : '#476BE7',
+                  backgroundColor: props.disabled ? 'transparent' : 'rgba(0, 84, 202, 0.16)'
                 }
               }}
             >
@@ -130,14 +132,14 @@ const ImageUploadInput = ({ name, label, ...props }: any & FieldProps & TextFiel
                   p: 2
                 }}
               >
-                <CloudUpload sx={{ fontSize: 40, color: 'text.secondary' }} />
+                <CloudUploadOutlined sx={{ fontSize: 20, color: (theme) => theme.palette.primary.main }} />
                 <Typography variant="body2" color="text.secondary" textAlign="center">
                   {t('core.label.addImage')}
                 </Typography>
 
               </Box>
             </Paper>
-
+            <Typography variant='caption'>{t('core.label.medida')}: {fileTypes(t).find(e => e.value === typeUpload)?.size.w} x {fileTypes(t).find(e => e.value === typeUpload)?.size.h} px. PNG.</Typography>
           </Box>
 
         )}
