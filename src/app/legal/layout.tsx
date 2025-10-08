a460d89e2e2097aba854bcb4f2914fc3137762cc
Your Name <yasielpv@sd.sd>
2025-10-08 10:45:31 -0400

    add fix term pages

--- src/app/legal/layout.tsx
+++ src/app/legal/layout.tsx
@@ -7,10 +7,8 @@ export default function Layout({
     children: React.ReactNode;
 }) {
     return (
-        <PublicLayout>
-            
-               {children}
-           
+        <PublicLayout>            
+               {children}           
         </PublicLayout>
     );
 }
