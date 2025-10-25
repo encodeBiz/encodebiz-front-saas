3cd4d1b41a11b9723814d899ffe907a39a5c157f
Your Name <yasielpv@sd.sd>
2025-10-24 22:30:39 -0400

    add fix issues

--- src/components/common/forms/fields/SelectCreatableInput.tsx
+++ src/components/common/forms/fields/SelectCreatableInput.tsx
@@ -40,7 +40,7 @@ const SelectCreatableInput: React.FC<SelectCreatableInputProps> = ({
         // Suggest the creation of a new value
         const isExisting = options.some((option) => inputValue?.toLowerCase() === option?.toLowerCase());
         if (inputValue !== '' && !isExisting) {
-          filtered.push(`Add "${inputValue}"`);
+          filtered.push(`${inputValue}`);
         }
 
         return filtered;
