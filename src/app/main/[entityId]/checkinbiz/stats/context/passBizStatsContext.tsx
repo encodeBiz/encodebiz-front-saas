07240ebced057f3f6fc2ae3d78591e933c541bdd
Your Name <yasielpv@sd.sd>
2025-10-30 17:40:41 -0400

    add panel and fix

---
+++ src/app/main/[entityId]/checkinbiz/stats/context/passBizStatsContext.tsx
@@ -0,0 +1,109 @@
+'use client'
+import React, { createContext, useContext, useState } from 'react';
+import { IPassIssuedStatsRequest } from '../model/PassIssued';
+import { IPassValidatorStatsRequest } from '../model/PassValidator';
+import { IPassTrendStatsRequest } from '../model/PassTrend';
+
+
+
+
+interface IPassinBizStatsProps {
+    payloadPassIssued: IPassIssuedStatsRequest | undefined,
+    payloadPassIssuedFilter: IPassIssuedStatsRequest | undefined,
+    setPayloadPassIssued: (payloadPassIssued: IPassIssuedStatsRequest) => void
+
+    payloadPassValidator: IPassValidatorStatsRequest | undefined,
+    payloadPassValidatorFilter: IPassValidatorStatsRequest | undefined,
+    setPayloadPassValidator: (payloadPassIssued: IPassValidatorStatsRequest) => void
+
+    payloadPassTrend: IPassTrendStatsRequest | undefined,
+    setPayloadPassTrend: (payloadPassIssued: IPassTrendStatsRequest) => void
+
+    lastUseFilter: any,
+    setLastUseFilter: (lastUseFilter: any) => void
+
+    error: any,
+    setError: (error: any) => void
+
+
+    graphData: any,
+    setGraphData: (graphData: any) => void
+
+    pending: any,
+    setPending: (graphData: any) => void
+
+    seriesChart2: Array<{ id: string, name: string }>,
+    setSeriesChart2: (payloadPassIssued: Array<{ id: string, name: string }>) => void
+
+    applyFilter: (type: "issued" | "validator" | "trend") => void
+}
+
+export const PassinBizStatsContext = createContext<IPassinBizStatsProps | undefined>(undefined);
+
+
+
+export function PassinBizStatsProvider({ children }: { children: React.ReactNode }) {
+
+    const [payloadPassIssued, setPayloadPassIssued] = useState<IPassIssuedStatsRequest>({
+        "dateRange": {
+            start: new Date(new Date().getFullYear(), 0, 1),
+            end: new Date()
+        },
+        "groupBy": "month",
+        "type": "",
+        "passStatus": "active",
+    } as IPassIssuedStatsRequest)
+    const [payloadPassValidator, setPayloadPassValidator] = useState<IPassValidatorStatsRequest>({
+
+        "dateRange": {
+            start: new Date(new Date().getFullYear(), 0, 1),
+            end: new Date()
+        },
+        "groupBy": "month",
+        "type": "",
+        "passStatus": "active",
+
+
+    } as IPassValidatorStatsRequest)
+    const [payloadPassTrend, setPayloadPassTrend] = useState<IPassTrendStatsRequest>()
+
+    const [seriesChart2, setSeriesChart2] = useState<Array<{ id: string, name: string }>>([])
+
+    const [payloadPassIssuedFilter, setPayloadPassIssuedFilter] = useState<IPassIssuedStatsRequest>()
+    const [payloadPassValidatorFilter, setPayloadPassValidatorFilter] = useState<IPassValidatorStatsRequest>()
+
+    const [error, setError] = useState<any>({ 'issued': '', 'validator': '' })
+
+    const [lastUseFilter, setLastUseFilter] = useState<any>({ 'issued': {}, 'validator': {} })
+    const [graphData, setGraphData] = useState<any>({ 'issued': {}, 'validator': {} })
+
+    const [pending, setPending] = useState<any>({ 'issued': false, 'validator': false })
+
+
+    const applyFilter = (type: "issued" | "validator" | "trend") => {
+        switch (type) {
+            case "issued":
+                setPayloadPassIssuedFilter(payloadPassIssued)
+                break;
+            case "validator":
+                setPayloadPassValidatorFilter(payloadPassValidator)
+                break;
+            default:
+                break;
+        }
+    }
+    return (
+        <PassinBizStatsContext.Provider value={{ error, setError, graphData, pending, setPending, setGraphData, lastUseFilter, setLastUseFilter, applyFilter, payloadPassIssuedFilter, payloadPassValidatorFilter, seriesChart2, payloadPassTrend, setPayloadPassTrend, setSeriesChart2, payloadPassIssued, setPayloadPassIssued, payloadPassValidator, setPayloadPassValidator }}>
+            {children}
+        </PassinBizStatsContext.Provider>
+    );
+}
+
+
+export const usePassinBizStats = () => {
+    const context = useContext(PassinBizStatsContext);
+    if (!context) {
+        throw new Error("usePassinBizStats must be used within an PassinBizStatsContext");
+    }
+    return context;
+};
---
+++ src/app/main/[entityId]/checkinbiz/stats/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+import { Container } from '@mui/material';
+import { useTranslations } from "next-intl";
+
+import HeaderPage from '@/components/features/dashboard/HeaderPage/HeaderPage';
+ import { PassinBizStatsProvider } from './context/passBizStatsContext';
+
+export default function HolderList() {
+  const t = useTranslations();
+
+
+  return (
+    <PassinBizStatsProvider>
+      <Container maxWidth="lg">
+        <HeaderPage
+          title={t("layout.side.menu.Stats")}
+        >
+ 
+        </HeaderPage>
+      </Container>
+    </PassinBizStatsProvider>
+  );
+}
