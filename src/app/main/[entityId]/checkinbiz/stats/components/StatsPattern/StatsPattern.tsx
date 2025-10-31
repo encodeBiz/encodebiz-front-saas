c856f9b4ef2c29fd7ff9e780704d333e8fc4e7ec
Your Name <yasielpv@sd.sd>
2025-10-30 21:12:32 -0400

    add fix

--- src/app/main/[entityId]/checkinbiz/stats/components/StatsPattern/StatsPattern.tsx
+++ src/app/main/[entityId]/checkinbiz/stats/components/StatsPattern/StatsPattern.tsx
@@ -4,26 +4,68 @@ import { Box, Typography } from '@mui/material';
 import { BorderBox } from '@/components/common/tabs/BorderBox';
 import { IBranchPattern } from '@/domain/features/checkinbiz/IStats';
 import { AccessTimeOutlined } from '@mui/icons-material';
+import { formatDay } from '@/lib/common/Date';
 
 export const StatsPatternCards = ({ data }: { data: IBranchPattern }) => {
- 
+
     return (
-        <Box display={'flex'} justifyContent={'flex-end'} alignItems='flex-end' gap={2} sx={{ width: '100%' }}>
-            <BorderBox sx={{ padding: 2 }}>
-                <Box display={'flex'} flexDirection={'row'}>
-                    <AccessTimeOutlined />
-                    <Box display={'flex'} flexDirection={'column'}>
-                        <Typography>Avg Start/End Hour</Typography>
-                        <Typography>Avg Start/End Hour</Typography>
+        <Box display={'flex'}   gap={2} sx={{ width: '100%' }}>
 
+            <Box display={'flex'} flexDirection={'column'}  gap={2}>
+                <BorderBox sx={{ padding: 2 }}>
+                    <Box display={'flex'} flexDirection={'row'} justifyContent={'center'} alignItems={'center'} gap={2}>
+                        <AccessTimeOutlined style={{ fontSize: 40 }} />
+                        <Box display={'flex'} flexDirection={'column'}>
+                            <Typography sx={{ fontSize: 20 }}>Avg Start/End Hour</Typography>
+                            <Typography sx={{ fontSize: 20 }}>{`${formatDay(Math.floor(data.avgStartHour))}:00 -> ${formatDay(Math.floor(data.avgEndHour))}:00`}</Typography>
+                        </Box>
                     </Box>
-                </Box>
+                </BorderBox>
 
-            </BorderBox>
+                <BorderBox sx={{ padding: 2 }}>
+                    <Box display={'flex'} flexDirection={'row'} justifyContent={'center'} alignItems={'center'} gap={2}>
+                        <AccessTimeOutlined style={{ fontSize: 40 }} />
+                        <Box display={'flex'} flexDirection={'column'}>
+                            <Typography sx={{ fontSize: 20 }}>Avg Cost Per Hour</Typography>
+                            <Typography sx={{ fontSize: 20 }}>{`${(Math.floor(data.dataPoints))}`}</Typography>
+                        </Box>
+                    </Box>
+                </BorderBox>
 
-            <BorderBox sx={{ padding: 2 }}>
+                <BorderBox sx={{ padding: 2 }}>
+                    <Box display={'flex'} flexDirection={'row'} justifyContent={'center'} alignItems={'center'} gap={2}>
+                        <AccessTimeOutlined style={{ fontSize: 40 }} />
+                        <Box display={'flex'} flexDirection={'column'}>
+                            <Typography sx={{ fontSize: 20 }}>Avg Cost Per Cycle</Typography>
+                            <Typography sx={{ fontSize: 20 }}>{`${(Math.floor(data.dataPoints))}`}</Typography>
+                        </Box>
+                    </Box>
+                </BorderBox>
+            </Box>
 
-            </BorderBox>
+            <Box display={'flex'} flexDirection={'column'}  gap={2}>
+                <BorderBox sx={{ padding: 2 }}>
+                    <Box display={'flex'} flexDirection={'row'} justifyContent={'center'} alignItems={'center'} gap={2}>
+                        <AccessTimeOutlined style={{ fontSize: 40 }} />
+                        <Box display={'flex'} flexDirection={'column'}>
+                            <Typography sx={{ fontSize: 20 }}>Avg Start/End Hour</Typography>
+                            <Typography sx={{ fontSize: 20 }}>{`${formatDay(Math.floor(data.avgStartHour))}:00 -> ${formatDay(Math.floor(data.avgEndHour))}:00`}</Typography>
+                        </Box>
+                    </Box>
+                </BorderBox>
+
+                <BorderBox sx={{ padding: 2 }}>
+                    <Box display={'flex'} flexDirection={'row'} justifyContent={'center'} alignItems={'center'} gap={2}>
+                        <AccessTimeOutlined style={{ fontSize: 40 }} />
+                        <Box display={'flex'} flexDirection={'column'}>
+                            <Typography sx={{ fontSize: 20 }}>Avg Cost Per Hour</Typography>
+                            <Typography sx={{ fontSize: 20 }}>{`${(Math.floor(data.dataPoints))}`}</Typography>
+                        </Box>
+                    </Box>
+                </BorderBox>
+
+                
+            </Box>
         </Box>
     );
 }
