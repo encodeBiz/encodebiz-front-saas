83757f35f3cb15e82361bc9f6bf41bf525734155
Jose <joseaceemdina@gmail.com>
2025-12-05 10:13:27 +0100

    Legales

--- src/components/common/forms/fields/SimpleCheck.tsx
+++ src/components/common/forms/fields/SimpleCheck.tsx
@@ -22,7 +22,7 @@ const SimpleCheck: React.FC<FieldProps & TextFieldProps> = ({
       }
       label={
         <Typography variant="body2">
-          I agree to the <Link href="#">Terms of Service</Link> and <Link href="#">Privacy Policy</Link>
+          I agree to the <Link href="#">Terms of Service</Link> and <Link href="/legal/privacy-policy" target='_blank'>Privacy Policy</Link>
         </Typography>
       }
       sx={{ mt: 2 }}
